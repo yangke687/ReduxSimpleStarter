02047668f1372cfc55a80e8c38279855a9d697be
Yang Ke <saint_yk@163.com>
2017-06-08 21:45:10 +0800

    lead in google developer api key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = "AIzaSyC4Iqc1wb5oW_8CcTlqBlV-w4wTtB2-11E";
+
 // create a new component
 // this component should produce some HTML
 const App = () => {
@@ -10,4 +12,4 @@ const App = () => {
 
 // take this component's generated HTML 
 // and put it on page (DOM)
-ReactDOM.render(<App />,document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
