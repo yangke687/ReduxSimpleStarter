7f076247668ee7fb0c5a5458a81ffb9c3a9f4fbd
Yang Ke <saint_yk@163.com>
2017-06-09 22:02:52 +0800

    custom styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<input 
 					value={this.state.term}
 					onChange={this.onInputChange.bind(this)} />
