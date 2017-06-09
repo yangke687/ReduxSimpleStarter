aa5ba02298d588d7c5e0e86153fb44662afd58e1
Yang Ke <saint_yk@163.com>
2017-06-09 14:09:01 +0800

    fetching youtube search results at the begining

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {
+	Component
+} from 'react';
 import ReactDOM from 'react-dom';
 import ytSearch from 'youtube-api-search';
 
@@ -6,19 +8,31 @@ const API_KEY = "AIzaSyC4Iqc1wb5oW_8CcTlqBlV-w4wTtB2-11E";
 
 import SearchBar from './components/search_bar';
 
-ytSearch({
-	key: API_KEY,
-	term: 'surfboards'
-}, function(res) {
-	console.log(res);
-});
-
 // create a new component
 // this component should produce some HTML
-const App = () => {
-	return (<div>
-		<SearchBar />
-	</div>);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: []
+		};
+		// fetching searching results
+		ytSearch({
+			key: API_KEY,
+			term: 'surfboards',
+		}, function(videos) {
+			this.setState({
+				videos
+			});
+		}.bind(this));
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 
