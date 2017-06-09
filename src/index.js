4b0406f30bae4b9b07441e6fc0c21f7c5731d85e
Yang Ke <saint_yk@163.com>
2017-06-09 11:38:31 +0800

    importing youtube-api-search and fetching some searching data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ytSearch from 'youtube-api-search';
 
 const API_KEY = "AIzaSyC4Iqc1wb5oW_8CcTlqBlV-w4wTtB2-11E";
 
 import SearchBar from './components/search_bar';
+
+ytSearch({
+	key: API_KEY,
+	term: 'surfboards'
+}, function(res) {
+	console.log(res);
+});
 
 // create a new component
 // this component should produce some HTML
