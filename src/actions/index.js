8964ec49d128982c0e8472c4af9229877175dfea
Yang Ke <saint_yk@163.com>
2017-06-11 16:10:06 +0800

    ajax requesting with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = '6a43408edafeadd664c7f1fffc7abf90';
+
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	// return promise 
+	const request = axios.get(url);
+
+	// return promise as a action payload
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
