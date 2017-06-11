a3ae7feb813cf9456db53b5e41798e6e4165ff13
Yang Ke <saint_yk@163.com>
2017-06-11 11:31:41 +0800

    binding searchBar onInputChange event

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,10 +3,28 @@ import React, {
 } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ''
+		};
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+	onInputChange(evt) {
+		const term = evt.target.value;
+		this.setState({
+			term: term
+		});
+	}
 	render() {
 		return (
 			<div className="input-group">
-				<input />
+				<input 
+					placeholder="Get a five day forcast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange = {this.onInputChange}
+					/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
