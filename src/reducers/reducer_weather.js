cc236a8cb23625b8d41311ac054d665d5fc58379
Yang Ke <saint_yk@163.com>
2017-06-11 22:31:22 +0800

    return mutation state of weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,13 @@
-export default function(state = null, action) {
-	console.log('Action received', action);
+import {
+	FETCH_WEATHER
+} from '../actions/index';
+
+export default function(state = [], action) {
+	//console.log('Action received', action);
+	switch (action.type) {
+		case FETCH_WEATHER:
+			return [action.payload.data, ...state]; // [city,city,city] NOT [city,[city,city]]
+	}
+	// empty
 	return state;
 }
