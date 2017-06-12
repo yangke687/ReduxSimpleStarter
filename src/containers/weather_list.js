99d795691c5acc64c48dddd40984d1c2e4c5c59c
Yang Ke <saint_yk@163.com>
2017-06-12 21:31:26 +0800

    show weahter data by react-sparklines charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,12 +6,35 @@ import {
 	connect
 } from 'react-redux';
 
+import {
+	Sparklines,
+	SparklinesLine
+} from 'react-sparklines';
+
 class WeatherList extends Component {
 	renderWeather(cityData) {
+		//console.log(cityData);
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 		return (
 			<tr key={cityData.city.name}>
 				<td>{cityData.city.name}</td>
-				<td></td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
+				<td>
+					<Sparklines height={120} width={180} data={pressures}>
+						<SparklinesLine color="green" />
+					</Sparklines>
+				</td>
+				<td>
+					<Sparklines height={120} width={180} data={humidities}>
+						<SparklinesLine color="blue" />
+					</Sparklines>
+				</td>
 			</tr>
 		);
 	}
