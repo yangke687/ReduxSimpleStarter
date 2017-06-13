7b53a0ba628a8c9d4fbc3204caaa13e9b4211816
Yang Ke <saint_yk@163.com>
2017-06-13 16:39:20 +0800

    add index action

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {
+	Provider
+} from 'react-redux';
+import {
+	createStore,
+	applyMiddleware
+} from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
+import {
+	BrowserRouter,
+	Route
+} from 'react-router-dom';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import PostsIndex from './components/posts_index';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+	<BrowserRouter>
+		<div>
+			<Route path='/' component={PostsIndex} />
+		</div>
+	</BrowserRouter>
+  </Provider>, document.querySelector('.container'));
