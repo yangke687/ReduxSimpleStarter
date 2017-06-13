87277449a689e8846d6574a1080ce7e61609587c
Yang Ke <saint_yk@163.com>
2017-06-13 16:59:48 +0800

    implementing posts reducer

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,15 @@
+import {
+	FETCH_POSTS
+} from '../actions';
+
+import _ from 'lodash';
+
+export default function(state = {}, action) {
+	switch (action.type) {
+		case FETCH_POSTS:
+			return _.mapKeys(action.payload, 'id');
+			break;
+		default:
+			return state;
+	}
+}
