7a9cf16a630bd6188f2050c14734ac9750ef4213
Ke Yang <yangke687@gmail.com>
2017-06-17 17:13:48 +0800

    eslint style

--- src/index.js
+++ src/index.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-	Provider
-} from 'react-redux';
-import {
-	createStore,
-	applyMiddleware
-} from 'redux';
-
-import reducers from './reducers';
-import {
-	BrowserRouter,
-	Route,
-	Switch
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
-	<BrowserRouter>
-		<div className="container">
-			<Switch>
-				<Route path='/posts/new' component={PostsNew} />
-				<Route path='/' component={PostsIndex} />
-			</Switch>
-		</div>
-	</BrowserRouter>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>, document.querySelector('.container'));
