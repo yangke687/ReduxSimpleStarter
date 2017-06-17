e46786d4d05ab791eaec865d955954e9642f81a6
Ke Yang <yangke687@gmail.com>
2017-06-17 16:22:02 +0800

    eslint style

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,54 +1,43 @@
-import React, {
-	Component
-} from 'react';
-
-import {
-	fetchPosts
-} from '../actions';
-
-import {
-	connect
-} from 'react-redux';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import _ from 'lodash';
-
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
-	componentDidMount() {
-		// fetching posts data
-		this.props.fetchPosts();
-	}
-	renderPosts() {
-		return _.map(this.props.posts, post => {
-			return (
-				post.title && <li className="list-group-item" key={post.id}>{post.title}</li>
-			);
-		});
-	}
-	render() {
-		return (
-			<div>
-				<div className="text-xs-right">
-					<Link className="btn btn-primary" to="/posts/new">
-						Add a Post
-					</Link>
-				</div>
-				<h3>Posts</h3>
-				<ul>
-					{this.renderPosts()}
-				</ul>
-			</div>
-		);
-	}
+  componentDidMount() {
+    // fetching posts data
+    this.props.fetchPosts();
+  }
+  renderPosts() {
+    return _.map(this.props.posts,
+      post => post.title && <li className="list-group-item" key={post.id}>{post.title}</li>);
+  }
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">Add a Post</Link>
+        </div>
+        <h3>Posts</h3>
+        <ul>
+          {this.renderPosts()}
+        </ul>
+      </div>
+    );
+  }
 }
+
+PostsIndex.propTypes = {
+  fetchPosts: PropTypes.func,
+  posts: PropTypes.object
+};
 
 function mapStateToProps(state) {
-	return {
-		posts: state.posts
-	};
+  return {
+    posts: state.posts,
+  };
 }
 
-export default connect(mapStateToProps, {
-	fetchPosts
-})(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
