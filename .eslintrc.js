f68f481ff9c81359eda542c8da9ee6bfeba220b6
Ke Yang <yangke687@gmail.com>
2017-06-17 15:53:15 +0800

    add eslint type checker

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "linebreak-style": "off",
+        "react/require-default-props": "off",
+        "class-methods-use-this": "off",
+        "react/jsx-filename-extension": "off",
+        "react/jsx-no-bind": "off",
+        "react/forbid-prop-types": "off"
+    }
+};
