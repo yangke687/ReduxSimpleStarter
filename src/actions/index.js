8a6ea014d31608779580313675d1117d19c1ad0b
Ke Yang <yangke687@gmail.com>
2017-06-17 17:47:01 +0800

    receiving new post and merge it into old state

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123#!321@';
@@ -20,4 +21,12 @@ export function createPost(values, cb) {
     type: CREATE_POST,
     payload: request,
   };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
+    payload: request,
+  }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,14 @@
-import { FETCH_POSTS} from '../actions'
+import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
-import _ from 'lodash'
-
-export default function(state = {} , action) {
+export default function (state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, 'id')
-      break
+      return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      return { ...state, [action.payload.data.id]: action.payload.data };
     default:
-      return state
+      return state;
   }
 }
+
