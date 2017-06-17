f8a66eee01003c7513910ad11eaf434a1d7e6b42
Ke Yang <yangke687@gmail.com>
2017-06-17 16:00:17 +0800

    add callback function invoker

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=123';
+const API_KEY = '?key=123#!321@';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -13,8 +13,9 @@ export function fetchPosts() {
     payload: request,
   };
 }
-export function createPost(values) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, cb) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => cb());
   return {
     type: CREATE_POST,
     payload: request,
